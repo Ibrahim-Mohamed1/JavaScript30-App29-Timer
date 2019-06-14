4825972f58ed50010cd659a923edd9c53d8f2eb3
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-06-13 20:09:24 -0600

    update

--- app.js
+++ app.js
@@ -24,3 +24,10 @@ function timer(seconds) {
     }, 1000);
 }
 
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+
+}
+
