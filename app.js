843ae5c4f03b5fd55cbb31895861705bfbc28a5c
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-06-13 20:10:16 -0600

    update

--- app.js
+++ app.js
@@ -48,4 +48,8 @@ function startTimer() {
 buttons.forEach(button => button.addEventListener('click', startTimer));
 document.customForm.addEventListener('submit', function (e) {
     e.preventDefault();
+    const mins = this.minutes.value;
+    console.log(mins);
+    timer(mins * 60);
+    this.reset();
 });
