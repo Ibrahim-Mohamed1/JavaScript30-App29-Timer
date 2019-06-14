8610777e3178fb51a88f8f1f4a94466cba7167de
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-06-13 20:08:55 -0600

    update

--- app.js
+++ app.js
@@ -3,3 +3,10 @@ const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector('.display__end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
+function timer(seconds) {
+    // clear any existing timers
+    clearInterval(countdown);
+
+
+}
+
